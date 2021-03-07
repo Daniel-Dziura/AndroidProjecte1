5dfb0a4c82170bfcf79ae9ecab4b3d1b030db4d3
xexe <francesccoll@paucasesnovescifp.cat>
2021-03-07 17:44:10 +0100

    SharedPreferences funcionando

--- app/src/main/java/com/example/android_projecte/AddGame.java
+++ app/src/main/java/com/example/android_projecte/AddGame.java
@@ -2,15 +2,18 @@ package com.example.android_projecte;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class AddGame extends AppCompatActivity {
-
+    private String PREFS_KEY = "mispreferencias";
     public EditText eTitol, eAny, eCat, eVal, eDir;
     public Button bt;
 
@@ -26,6 +29,12 @@ public class AddGame extends AppCompatActivity {
         eVal = findViewById(R.id.editTextVal);
         eDir = findViewById(R.id.editTextDesar);
         bt = findViewById(R.id.addGame);
+
+        eTitol.setText(getValue(getApplicationContext(), "tit"));
+        eCat.setText(getValue(getApplicationContext(), "cat"));
+        eAny.setText(getValue(getApplicationContext(), "any"));
+        eVal.setText(getValue(getApplicationContext(), "val"));
+        eDir.setText(getValue(getApplicationContext(), "dir"));
 
         final Game game = new Game(11,"","", "", "", R.drawable.imgdefault, "", "", "https://store.steampowered.com/?l=spanish", "N/A", "https://www.g2a.com/", "N/A", "https://www.instant-gaming.com/es/", "N/A");
 
@@ -54,5 +63,100 @@ public class AddGame extends AppCompatActivity {
                 finish();
             }
         });
+
+        eTitol.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                saveValue(getApplicationContext(), s.toString(), "tit");
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+        eCat.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                saveValue(getApplicationContext(), s.toString(),"cat");
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+        eAny.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                saveValue(getApplicationContext(), s.toString(), "any");
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+        eVal.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                saveValue(getApplicationContext(), s.toString(), "val");
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+        eDir.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                saveValue(getApplicationContext(), s.toString(), "dir");
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
     }
+
+    public void saveValue(Context context, String text, String key) {
+        SharedPreferences settings = context.getSharedPreferences(PREFS_KEY, MODE_PRIVATE);
+        SharedPreferences.Editor editor;
+        editor = settings.edit();
+        editor.putString(key, text);
+        editor.commit();
+    }
+
+    public String getValue(Context context, String key) {
+        SharedPreferences preferences = context.getSharedPreferences(PREFS_KEY, MODE_PRIVATE);
+        return  preferences.getString(key, "");
+    }
+
 }
